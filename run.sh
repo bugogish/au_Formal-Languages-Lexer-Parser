bb621d762c4bf673bce3b92db4142c03dd8d4dd1
bugogish <bugogish@gmail.com>
2017-04-06 18:38:13 +0300

    quick fixed

--- run.sh
+++ run.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
-#./gradlew build
-#a=\'
-#if [ $# -gt 1 ]; then
-#    FILE=$1
-#    if [ "$2" == "-filter" ] || [ "$2" == "-f" ] || [ "$2" == "--filter" ]; then
-#        FILTER="-filter"
-#        ./gradlew run -PappArgs="[$a$FILE$a, $a$FILTER$a]"
-#    fi
-#else
-#    FILE=$1
-#    ./gradlew run -PappArgs="[$a$FILE$a]"
-#fi
-
 ./gradlew build
 args=""
 
